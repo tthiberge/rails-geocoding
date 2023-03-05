d7c55a85509448d2611272029cd80a58f0612dd1
Theophile Thiberge <tthiberge@gmail.com>
2023-03-05 12:22:59 +0100

    testing geocoding over - some questions for the TA - starting the maps

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'rides/index'
+  get 'rides/show'
+  resources :flats do
+    get '/test-forms', to: "flats#tests_form"
+  end
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :rides
+
 end
